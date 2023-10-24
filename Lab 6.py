ec8d3dc8ecbeff14f424b12379b5f31644ed474b
Tara Menon <taramenon@Taras-MBP.lan>
2023-10-24 18:38:30 -0400

    Tara"s addition to the repo! Also, resolved merge issues again in Lab 6.py

--- Lab 6.py
+++ Lab 6.py
@@ -11,8 +11,12 @@ def encode_pass(password):
         encode_pass += add3
     return encode_pass
 
-def decoded_password(password):
-    pass
+def decode_pass(encode_pass):
+    decode_pass = ""
+    for i in encode_pass:
+        subtract3 = str((int(i) - 3) % 10)
+        decode_pass += subtract3
+    return decode_pass
 
 def main():
 
@@ -20,26 +24,24 @@ def main():
         print("\nMenu:")
         print("------------")
         print("1. Encode\n2. Decode\n3. Quit")
-        option = int(input("Select a Menu Option:"))
+        option = int(input("Select a Menu Option: "))
 
         if option == 1:
-            password = str(input("Please enter your password to encode:"))
+            password = str(input("Please enter your password to encode: "))
             if len(password) == 8 and password.isdigit():
                 encoded_pass = encode_pass(password)
-                print("Your password has been encoded and stored!")
+                print("Your password has been encoded and stored!\n")
             else:
                 print("Please enter a valid 8-digit password.\n")
-        if option == 2:
+        elif option == 2:
             if encode_pass:
-                print(f'The encoded password is {encoded_pass}, and the original password is {password}. \n')
+                print(f'The encoded password is {encoded_pass}, and the original password is {decode_pass(encoded_pass)}. \n')
             else:
                 print("You need a password encoded first!\n")
-
-            print(decoded_password(password))
         elif option == 3:
             break
-    else:
-        print("Please input a valid  menu selection.\n")
+        else:
+            print("Please input a valid  menu selection.\n")
 
 if __name__ == '__main__':
     main()
